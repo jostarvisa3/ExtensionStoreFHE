59aea1ed67bb46cde17044739575d481b5c9e858
jostarvisa3 <evgeniyalomuga@mail.ru>
2025-12-04 15:21:41 +0800

    add SepoliaConfig.dbg.json, helpers.jsx, build-model-decorator.jsx

---
+++ frontend/web/src/build-shared-adaptive/build-model-decorator.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default function customer-inventory() { return <div>Component</div>; }
---
+++ frontend/web/src/helpers.jsx
@@ -0,0 +1,3 @@
+export const config = { runtime: 'edge' };
+
+export default function sms-analytics-util() { return <div>Component</div>; }
